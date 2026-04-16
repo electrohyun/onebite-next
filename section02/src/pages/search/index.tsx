47a7a17462c19329e35b34a412ba27cb16c3d66f
electrohyun <dev.electrohyun@gmail.com>
2026-04-16 13:25:20 +0900

    2.20까지

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,11 +1,47 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
-import type { ReactNode } from "react";
+import fetchBooks from "@/lib/fetch-books";
+import { BookData } from "@/types";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState, type ReactNode } from "react";
+
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+
+//   return {
+//     props: { books },
+//   };
+// };
 
 export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
+      <Head>
+        <title>한입 북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입 북스 - 검색 결과" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요!"
+        />
+      </Head>
       {books.map((book) => {
         return <BookItem key={book.id} {...book} />;
       })}
